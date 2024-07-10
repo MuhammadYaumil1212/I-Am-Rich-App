5ad202f3ecf7ad33be0774c9b47e2c285ef2e529
myaumitramadhani <yaumilramkhadani@gmail.com>
2024-07-10 18:37:20 +0700

    Initial Commit

---
+++ I Am Rich App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich App
+//
+//  Created by M Yaumit Ramadhani on 10/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
